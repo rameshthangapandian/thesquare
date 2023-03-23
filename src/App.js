2c377eb9c9ec2c348d16012176fcfea585124404
rameshpl <rameshrb@provenlogic.net>
2023-03-23 17:57:10 +0530

    Adding all changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState("");
+  const [boxes, setBoxes] = useState([]);
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setNumber(value);
+    const numBoxes = value ** 2;
+    const newBoxes = Array.from({ length: numBoxes }, (_, i) => i + 1);
+    setBoxes(newBoxes);
+  };
+
+  const containerStyle = {
+    display: "grid",
+    gridTemplateColumns: `repeat(${number}, 1fr)`,
+    gridTemplateRows: `repeat(${number}, 1fr)`,
+    gridGap: "5px",
+    marginTop: "10px",
+  };
+
+  const boxStyle = {
+    width: "50px",
+    height: "50px",
+    border: "1px solid black",
+    margin: "5px",
+    boxSizing: "border-box",
+    background: "burlywood"
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>The Square Box</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter the number in the text box
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="number" className="e-input" value={number} onChange={handleInputChange} />
+        <div style={containerStyle}>
+        {boxes.map((box) => (
+          <div key={box} style={boxStyle} />
+        ))}
+        </div>
+          </header>
     </div>
   );
 }
